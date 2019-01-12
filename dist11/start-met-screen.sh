63b661b5180a5d45bba8b6860add2902503c1610
Bas Heijermans <bas@heppen.be>
2019-01-12 16:47:35 +0100

    Add files via upload

--- dist11/start-met-screen.sh
+++ dist11/start-met-screen.sh
@@ -1,28 +1,38 @@
 #!/bin/bash
 
+
 killall -KILL websdr64
-killall -KILL rtl_tcp
-killall -KILL rtl_tcp
+killall -s 9 rsp_tcp
+killall -s 9 rsp_tcp
+killall -s 9 rsp_tcp
+
 
 cd /opt/websdr/dist11
-### Direct sampling setting, niet nodig met upconverter
-## screen -dmS rtl_tcp rtl_tcp -D 2
 
-### Normale sampling met up-converter
-screen -dmS rtl_tcp80m rtl_tcp -p 1234 -d 1
-
-### 2m Dongle
-screen -dmS rtl_tcp2m rtl_tcp -p 1235 -d 0
+### RSP1A rules
+sleep 1
+screen -dmS rsp_tcp1 taskset -c 0 rsp_tcp -d1 -p1234 -r60 -A-30 -L -N -n1500
+sleep 1
+screen -dmS rsp_tcp2 taskset -c 0 rsp_tcp -d2 -p1235 -r60 -A-30 -L -N -n1500
+sleep 1
+screen -dmS rsp_tcp3 taskset -c 0 rsp_tcp -d3 -p1236 -r60 -A-30 -L -N -n1500
 
 sleep 4
-screen -dmS websdr ./websdr64
+nice -n -10 screen -dmS websdr taskset -c 1 ./websdr64
 
-### Nodig vanwege bug in websdr met progfreq + upconverter
-sleep 4
-killall -KILL rtl_tcp
+### Needed for upconverter
+##### sleep 120 needed for FFT = 2 value
+sleep 6
+killall -s9 rsp_tcp
+killall -s9 rsp_tcp
+killall -s9 rsp_tcp
 
-screen -dmS rtl_tcp80m rtl_tcp -g 18 -p 1234 -d 1
-screen -dmS rtl_tcp2m rtl_tcp  -g 38 -p 1235 -d 0
+sleep 1
+screen -dmS rsp_tcp0-2 taskset -c 0 rsp_tcp -p1234 -L -d1 -N -n1500
+sleep 1
+screen -dmS rsp_tcp2-5 taskset -c 0 rsp_tcp -p1235 -L -d2 -N -n1500
+sleep 1
+screen -dmS rsp_tcp5-8 taskset -c 0 rsp_tcp -p1236 -L -d3 -N -n1500
 
 exit
 
